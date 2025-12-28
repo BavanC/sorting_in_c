9c78ae7d19e9ccd58f4637dff1bb6c85dfa59278
Bav C <bc463@cantab.ac.uk>
2025-12-28 17:13:52 +0000

    moving unsorted inputs into ignored directory

--- include/fileread.h
+++ include/fileread.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <arraycursor.h>
 
-#define LIST_FILE "unsorted_numbers.txt"
+#define LIST_FILE "unsorted_lists/unsorted_numbers.txt"
 #define LIST_SIZE 25
 
 int read_file_into_array(FILE *fptr, ArrayCursor *intlist_cursor);
